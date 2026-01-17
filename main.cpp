61cf156dae1864f16b2d8faee6af10de170f576e
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-01-17 09:32:05 +0100

    Removed comments from main and UI

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-//#include "Gameplay/Gameplay.hpp"
 #include "UI/UI.hpp"
 
 int main(int argc, char* argv[])
@@ -6,7 +5,6 @@ int main(int argc, char* argv[])
     UI ui(argc, argv);
     ui.showMainWindow();
     ui.RunExec();
-    //Gameplay{}.Run();
 
     return 0;
 };
