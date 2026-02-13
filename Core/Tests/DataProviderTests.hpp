a72f78da355f40a4e887c676c6ec3da2d7b6ea6e
Bartosz Lozinski <lozinski.bart@gmail.com>
2026-02-13 18:55:21 +0100

    Removed unnecessary DataProcessor lib

--- Core/Tests/DataProviderTests.hpp
+++ Core/Tests/DataProviderTests.hpp
@@ -4,96 +4,11 @@
 #include "Gameplay/DataProvider.hpp"
 #include "Game/Bird.hpp"
 #include "Game/Pipes.hpp"
-#include "DataProcessor/DataExporter.hpp"
 
 class DataProviderFixture : public testing::Test
 {
 public:
     Gameplay::DataProvider dataProvider;
-    DataExporter dataExporter{"test_data.csv", "Core/Tests/"};
-
-    void TearDown() override;
 };
 
-TEST_F(DataProviderFixture, WriteHeaderTest)
-{
-    dataExporter.WriteHeader(dataProvider);
-
-    std::ifstream file{"Core/Tests/test_data.csv"};
-    std::string headerLine;
-    std::getline(file, headerLine);
-
-    EXPECT_EQ(headerLine, dataProvider.GetHeader());
-};
-
-TEST_F(DataProviderFixture, ExportRowTest)
-{
-    Game::Bird bird;
-    Game::Pipes pipes;
-
-    dataProvider.SetState(bird, pipes);
-    dataExporter.ExportRow(dataProvider);
-
-    std::ifstream file{"Core/Tests/test_data.csv"};
-    std::string dataLine;
-    std::getline(file, dataLine);
-
-    EXPECT_EQ(dataLine, dataProvider.GetRow());
-};
-
-TEST_F(DataProviderFixture, ExportRowWithHeaderTest)
-{
-    dataExporter.WriteHeader(dataProvider);
-
-    Game::Bird bird;
-    Game::Pipes pipes;
-
-    dataProvider.SetState(bird, pipes);
-    dataExporter.ExportRow(dataProvider);
-
-    std::ifstream file{"Core/Tests/test_data.csv"};
-    std::string headerLine;
-    std::getline(file, headerLine);
-    std::string dataLine;
-    std::getline(file, dataLine);
-
-    EXPECT_EQ(headerLine, dataProvider.GetHeader());
-    EXPECT_EQ(dataLine, dataProvider.GetRow());
-}
-
-TEST_F(DataProviderFixture, Export3RowsWithHeaderTest)
-{
-    dataExporter.WriteHeader(dataProvider);
-    static constexpr unsigned expectedLines = 4;
-
-    Game::Bird bird;
-    Game::Pipes pipes;
-
-    for (unsigned i = 0; i < (expectedLines - 1); ++i)
-    {
-        dataProvider.SetState(bird, pipes);
-        dataExporter.ExportRow(dataProvider);
-        bird.UpdateState();
-        pipes.UpdateState();
-    }
-
-    std::ifstream file{"Core/Tests/test_data.csv"};
-    std::string headerLine;
-    std::getline(file, headerLine);
-    unsigned lineCount = 0;    
-    
-    while (!file.eof())
-    {
-        std::string dataLine;
-        std::getline(file, dataLine);
-        lineCount++;
-    }
-
-    EXPECT_EQ(headerLine, dataProvider.GetHeader());
-    EXPECT_EQ(lineCount, expectedLines);
-}
-
-void DataProviderFixture::TearDown()
-{
-    std::filesystem::remove("Core/Tests/test_data.csv");
-}
+//TO DO REWRITE TESTS FOR DATAPROVIDER
